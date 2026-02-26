3974bb65eb23d89407ae5fd534aa3aae47b7c4ea
O-w-it <omar.abdu20187@gmail.com>
2026-02-27 02:08:22 +0300

    اضافة صفحة اضافة خصم

---
+++ resources/views/discount/addDiscount.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.master')
+@section('content')
+    <!-- Contact Section -->
+    <section id="contact" class="contact section">
+        <!-- Section Title -->
+        <div class="col-lg-8 offset-lg-2 text-center">
+            <div class="section-title">
+                <h3>Add <span class="orange-text">Discount</span></h3>
+            </div>
+        </div>
+
+        <div class="container my-5" style="direction: rtl; text-align: right">
+            <div class="row justify-content-center">
+
+                <div class="col-14 col-md-10 col-lg-8">
+
+                    <div class="form-container">
+                        <div style="display: flex;justify-content: space-between">
+                            <h3>نموذج إضافة خصم</h3>
+                            <a href="javascript:history.back()">
+                                <i class="bi bi-arrow-left fs-4 text-dark"></i>
+                            </a>
+                        </div>
+
+                        <form action="forms/contact.php" method="post">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="price" placeholder="" required=""
+                                    value="200" readonly>
+                                <label for="price">سعر المنتج</label>
+                            </div>
+
+                            <div class="form-floating mb-3">
+                                <input type="number" class="form-control" id="discount" name="discount"
+                                    placeholder="Subject" required="">
+                                <label for="discount">الخصم</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="number" class="form-control" id="discountPeriod" name="discountPeriod"
+                                    placeholder="Subject" required="">
+                                <label for="discountPeriod">مدة الخصم</label>
+                            </div>
+                            <div class="d-grid" style="direction: ltr">
+                                <button type="submit" class="btn-submit">إضافة <i class="bi bi-plus ms-2"></i></button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section><!-- /Contact Section -->
+@endsection
