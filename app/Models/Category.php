685f69424605cccd9356397ca8c03a1941e0178f
O-w-it <omar.abdu20187@gmail.com>
2026-03-08 01:03:19 +0300

    تطبيق الاضافة والحذف والعرض والتعديل على الاقسام

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = 'categories';
+
+    protected $primaryKey = 'cat_id';
+
+    protected $fillable = [
+        'cat_name'
+    ];
 }
