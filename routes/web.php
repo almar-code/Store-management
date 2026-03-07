7aa0584007d16f6c7f164a5bc1a5a98105508a3f
O-w-it <omar.abdu20187@gmail.com>
2026-03-08 01:03:19 +0300

    تطبيق الاضافة والحذف والعرض والتعديل على الاقسام

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,21 @@ use App\Http\Controllers\DiscountController;
 use App\Http\Controllers\SizeController;
 use App\Http\Controllers\ColorsController;
 use App\Http\Controllers\AdsController;
+use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('addproduct', [ProductController::class, 'AddProduct']);
 
-Route::get('addsection', [SectionController::class, 'AddSection']);
-Route::get('sections', [SectionController::class, 'Sections']);
+Route::get('/addsection', [SectionController::class, 'AddSection']);// صفحة إضافة قسم
+Route::get('/sections',[SectionController::class,'Sections']); // عرض الأقسام
+Route::post('/add-section',[SectionController::class,'store']);// إضافة قسم
+Route::get('/edit-section/{id}',[SectionController::class,'edit']);// تحميل نفس الصفحة الاضافة لكن مع بيانات القسم للتعديل
+Route::post('/update-section/{id}',[SectionController::class,'update']);// تعديل القسم
+Route::get('/delete-section/{id}',[SectionController::class,'destroy']);// حذف قسم
+
 Route::get('addcategorie', [CategorieController::class, 'AddCategorie']);
 Route::get('categorieManagement', [CategorieController::class, 'CategorieManagement']);
-
+Route::get('addproduct', [ProductController::class, 'AddProduct']);
 Route::get('products', [ProductController::class, 'Products']);
 Route::get('login', [LoginController::class, 'Login']);
 Route::get('orders', [OrdersController::class, 'Orders']);
