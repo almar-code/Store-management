a66321f713e6add584304d414ef2fd014157d6b6
ali <alftyhyly35@gmail.com>
2026-03-20 17:05:50 -0700

    تعديل زر الاضافه والتعديل في جميع الصفحات

--- resources/views/Products/addproduct.blade.php
+++ resources/views/Products/addproduct.blade.php
@@ -126,7 +126,7 @@
 
                             <div class="d-grid" style="direction: ltr">
                                 @if (!isset($editProduct))
-                                    <button type="submit" class="btn-submit" id="saveBtn">
+                                    <button type="button" class="btn-submit" id="saveBtn" onclick="activateLoading('dataForm', 'saveBtn');">
 
                                         إضافة <i class="bi bi-plus ms-2"></i>
 
@@ -136,7 +136,7 @@
 
                                 {{-- زر التعديل يظهر فقط في حالة التعديل --}}
                                 @if (isset($editProduct))
-                                    <button type="submit" class="btn-submit" id="saveBtn">
+                                    <button type="button" class="btn-submit" id="saveBtn" onclick="activateLoading('dataForm', 'saveBtn');">
 
                                         تعديل <i class="bi bi-pencil-square" style=" font-size:13px; margin: 3px"></i>
 
